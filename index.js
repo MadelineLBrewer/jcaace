ab6cb7dc4228ab269b906a741ec0ab44afec438b
Madeline Brewer <madelinebrewer95@gmail.com>
2022-11-16 12:54:45 -0700

    Save changes before adding sass file

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
+// Modal image homepage ///////////////////
 
-// Expand more services page
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// Expand more services page ///////////////////
 
 document.addEventListener('DOMContentLoaded', () => {
   const expandsMore = document.querySelectorAll('[expand-more]')
@@ -20,7 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 })
 
-// Expand more projects page
+// Expand more projects page //////////////
 
 function leedFunction(){
   var dots1 = document.getElementById("dots1");
@@ -69,3 +96,4 @@ function commercialFunction(){
     moreText3.style.display = "inline";
   }
 }
+
